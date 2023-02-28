dfa5ef4274001cf85ff97f8d5b1fc49585a6fa1a
ololoshka2871 <SweetTreasure@2ch.hk>
2023-02-28 10:42:53 +0300

    initial commit

---
+++ draw_resonator.py
@@ -0,0 +1,79 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from common import extend_matrix, build_transform_matrix, remove_extended_matrix, transform_all
+
+import generate_rezonator 
+
+
+def draw_object_ext_coords(vertexes, format="-", color='black'):
+    """
+    Функция рисует объект по заданным вершинам
+    :param vertexes: вершины объекта
+    :param color: цвет
+    :return: None
+    """
+    vertexes = remove_extended_matrix(vertexes)
+    return plt.plot(vertexes[:, 0], vertexes[:, 1], format, color=color)
+
+
+def draw_test():
+    # нарисовать базовую форму резонатора
+    # базовая точка - середина в месте крепления (0, 0)
+    rezonator = extend_matrix(generate_rezonator.rezonator)
+    
+    # первая ветка
+    target1 = extend_matrix(generate_rezonator.target1)
+
+    # вторая ветка
+    target2 = extend_matrix(generate_rezonator.target2)
+
+    # рабочая область
+    working_area = extend_matrix(generate_rezonator.working_area)
+    
+    # Запрещенная область
+    forbidden_area = extend_matrix(generate_rezonator.forbidden_area)
+    
+    # рисуем базовую форму
+    draw_object_ext_coords(rezonator, color='black', format='--')
+    draw_object_ext_coords(target1, color='black')
+    draw_object_ext_coords(target2, color='black')
+
+    # рисуем рабочую область
+    draw_object_ext_coords(working_area, color='blue', format='-.')
+
+    # рисуем запрещенную область
+    draw_object_ext_coords(forbidden_area, color='magenta', format='-')
+
+    #---------------------------
+
+    matrix = build_transform_matrix(angle=0, offset=(0.2, 0.5))
+
+    transformed = transform_all([rezonator, target1, target2, forbidden_area], matrix)
+
+    draw_object_ext_coords(transformed[0], color='red', format='--')
+    draw_object_ext_coords(transformed[1], color='red')
+    draw_object_ext_coords(transformed[2], color='red')
+    draw_object_ext_coords(transformed[3], color='red')
+
+    #---------------------------
+
+    matrix = build_transform_matrix(angle=-10, offset=(0.2, 0.5))
+
+    transformed = transform_all([rezonator, target1, target2, forbidden_area], matrix)
+
+    draw_object_ext_coords(transformed[0], color='green', format='--')
+    draw_object_ext_coords(transformed[1], color='green')
+    draw_object_ext_coords(transformed[2], color='green')
+    draw_object_ext_coords(transformed[3], color='green')
+
+    #---------------------------
+
+    # Установка одинакового масштаба по осям X и Y
+    plt.axis('equal')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    draw_test()
---
+++ generate_rezonator.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+from common import extend_matrix, remove_extended_matrix, transform_all
+
+# Константы
+REZONATOR_LENGTH = 4.5
+REZONATOR_HEIGHT = 1.0
+BRANCH_LENGTH = 1.96
+BRANCH_WIDTH = 0.38
+REZONATOR_HEIGHT_HALF = REZONATOR_HEIGHT / 2
+
+TARGET_WIDTH = 0.5
+TARGET_HEIGHT = 0.32
+TARGET_VERTICAL_OFFSET = 0.13
+TARGETS_STEP = 0.62
+
+TARGET_RIGHT_OFFSET = REZONATOR_HEIGHT_HALF - \
+    BRANCH_WIDTH + (BRANCH_WIDTH - TARGET_HEIGHT) / 2
+
+WORKING_AREA_Y_OFFSET = 3.67
+WORKING_AREA_WIDTH = 3.2
+WORKING_AREA_HEIGHT = 1.5
+WORKING_AREA_WIDTH_HALF = WORKING_AREA_WIDTH / 2
+
+FORBIDDEN_AREA_DOWN_OFFSET = 0.1
+FORBIDDEN_AREA_SIDE_OFFSET = 0.01
+FORBIDDEN_AREA_UP_OFFSET = 0.725
+
+# Резонатор
+rezonator = np.array([[0, 0], [REZONATOR_HEIGHT_HALF, 0],
+                      [REZONATOR_HEIGHT_HALF, REZONATOR_LENGTH],
+                      [REZONATOR_HEIGHT_HALF - BRANCH_WIDTH, REZONATOR_LENGTH],
+                      [REZONATOR_HEIGHT_HALF - BRANCH_WIDTH,
+                          REZONATOR_LENGTH - BRANCH_LENGTH],
+                      [-REZONATOR_HEIGHT_HALF + BRANCH_WIDTH,
+                          REZONATOR_LENGTH-BRANCH_LENGTH],
+                      [-REZONATOR_HEIGHT_HALF + BRANCH_WIDTH, REZONATOR_LENGTH],
+                      [-REZONATOR_HEIGHT_HALF, REZONATOR_LENGTH],
+                      [-REZONATOR_HEIGHT_HALF, 0],
+                      [0, 0]])
+
+# первая ветка
+target1 = np.array([[TARGET_RIGHT_OFFSET, REZONATOR_LENGTH - TARGET_VERTICAL_OFFSET - TARGET_WIDTH],
+                    [TARGET_RIGHT_OFFSET + TARGET_HEIGHT, REZONATOR_LENGTH -
+                        TARGET_VERTICAL_OFFSET - TARGET_WIDTH],
+                    [TARGET_RIGHT_OFFSET + TARGET_HEIGHT,
+                        REZONATOR_LENGTH - TARGET_VERTICAL_OFFSET],
+                    [TARGET_RIGHT_OFFSET, REZONATOR_LENGTH - TARGET_VERTICAL_OFFSET],
+                    [TARGET_RIGHT_OFFSET, REZONATOR_LENGTH - TARGET_VERTICAL_OFFSET - TARGET_WIDTH]])
+
+# матрица cмещения на TARGETS_STEP мм влево
+target_move_matrix = np.array([[1, 0, -TARGETS_STEP],
+                               [0, 1, 0],
+                               [0, 0, 1]])
+# вторая ветка
+target2 = (np.dot(target_move_matrix, extend_matrix(target1))[0:2, :]).T
+
+# рабочая область
+working_area = np.array([[-WORKING_AREA_WIDTH_HALF, WORKING_AREA_Y_OFFSET],
+                         [WORKING_AREA_WIDTH_HALF, WORKING_AREA_Y_OFFSET],
+                         [WORKING_AREA_WIDTH_HALF,
+                             WORKING_AREA_Y_OFFSET + WORKING_AREA_HEIGHT],
+                         [-WORKING_AREA_WIDTH_HALF,
+                             WORKING_AREA_Y_OFFSET + WORKING_AREA_HEIGHT],
+                         [-WORKING_AREA_WIDTH_HALF, WORKING_AREA_Y_OFFSET]])
+
+# Запрещенная область
+forbidden_area = np.array([[0, FORBIDDEN_AREA_DOWN_OFFSET],
+                           [REZONATOR_HEIGHT_HALF - FORBIDDEN_AREA_SIDE_OFFSET,
+                               FORBIDDEN_AREA_DOWN_OFFSET],
+                           [REZONATOR_HEIGHT_HALF - FORBIDDEN_AREA_SIDE_OFFSET,
+                               REZONATOR_LENGTH - FORBIDDEN_AREA_UP_OFFSET],
+                           [REZONATOR_HEIGHT_HALF - BRANCH_WIDTH + FORBIDDEN_AREA_SIDE_OFFSET,
+                               REZONATOR_LENGTH - FORBIDDEN_AREA_UP_OFFSET],
+                           [REZONATOR_HEIGHT_HALF - BRANCH_WIDTH + FORBIDDEN_AREA_SIDE_OFFSET,
+                               REZONATOR_LENGTH - BRANCH_LENGTH - FORBIDDEN_AREA_SIDE_OFFSET],
+                           [-REZONATOR_HEIGHT_HALF + BRANCH_WIDTH - FORBIDDEN_AREA_SIDE_OFFSET,
+                               REZONATOR_LENGTH - BRANCH_LENGTH - FORBIDDEN_AREA_SIDE_OFFSET],
+                           [-REZONATOR_HEIGHT_HALF + BRANCH_WIDTH - FORBIDDEN_AREA_SIDE_OFFSET,
+                               REZONATOR_LENGTH - FORBIDDEN_AREA_UP_OFFSET],
+                           [-REZONATOR_HEIGHT_HALF + FORBIDDEN_AREA_SIDE_OFFSET,
+                               REZONATOR_LENGTH - FORBIDDEN_AREA_UP_OFFSET],
+                           [-REZONATOR_HEIGHT_HALF + FORBIDDEN_AREA_SIDE_OFFSET,
+                               FORBIDDEN_AREA_DOWN_OFFSET],
+                           [0, FORBIDDEN_AREA_DOWN_OFFSET]])
