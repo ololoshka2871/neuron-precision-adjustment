9a434bc2c5d5360147a1916676b096530211c31c
ololoshka2871 <SweetTreasure@2ch.hk>
2023-03-29 11:35:31 +0300

    TimeLimit wraper

--- learn_v4.py
+++ learn_v4.py
@@ -7,6 +7,7 @@ from keras.optimizers import adam_legacy  # keras 2.12.0
 
 import tensorflow as tf
 import gymnasium as gym
+from gymnasium.wrappers import TimeLimit
 from misc.EnvBackCompability import EnvBackCompability
 
 import gym_quarz
@@ -16,9 +17,11 @@ from controllers.controller_v4 import NNController
 
 def learn_main(filename: str,
                steps: int,
+               max_episode_steps: int = 200,
                learning_rate=0.001) -> None:
 
     env = gym.make("gym_quarz/QuartzEnv-v3")
+    env = TimeLimit(env, max_episode_steps=max_episode_steps)
     env = EnvBackCompability(env)  # type: ignore
 
     dqn = NNController(env.observation_space, env.action_space)
@@ -47,11 +50,13 @@ if __name__ == '__main__':
 
     # parse argumants
     parser = argparse.ArgumentParser()
-    parser.add_argument('-m', type=int, help='Max iterations', default=100000)
+    parser.add_argument('-i', type=int, help='Max iterations', default=100000)
+    parser.add_argument('-s', type=float, help='Max steps', default=200)
     parser.add_argument(
         'file', type=str, help='Simulation history file', nargs='?', default='learn_v4.h5') # в .tf не сохраняет
     args = parser.parse_args()
 
     learn_main(args.file,
-               args.m,
+               args.i,
+               args.s,
                learning_rate=0.0005)
